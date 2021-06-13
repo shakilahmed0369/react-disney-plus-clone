04bf54ebf4450af26ec6830b3dbc6399ff8b8f05
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-06-13 20:22:24 +0600

    ADD: Detail page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
